4078b3b0335df73f46a23e3ecfeb1b4061a593e4
Giwang <giwang.risti@lidahbuayagroup.com>
2025-08-27 13:34:59 +0700

    update database

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -134,7 +134,7 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Histori Login Admin</h3>
+                <h3 class="card-title">Histori Login</h3>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
